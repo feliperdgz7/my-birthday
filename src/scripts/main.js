0acd7c6a21948e9ef7116e63ef3b3cc5c14469d8
Felipe Rodrigues <arthurdz741@gmail.com>
2024-02-29 20:38:39 -0300

    Ajust in const

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,22 @@
 AOS.init();
 
-const dataEvent = new Date("Jul 30, 2024 19:00:00");
-const timeStempEvent = dataEvent.getTime();
+let dataEvent = new Date("Jul 30, 2024 19:00:00");
+let timeStempEvent = dataEvent.getTime();
 
-const countTime = setInterval(function(){
-    const now = new Date();
-    const timeStempNow = now.getTime();
+let countTime = setInterval(function(){
+    let now = new Date();
+    let timeStempNow = now.getTime();
 
-    const distanceEvent = timeStempEvent - timeStempNow;
+    let distanceEvent = timeStempEvent - timeStempNow;
 
-    const dayInMs = 1000 * 60 * 60 *24;
-    const hourInMs = 1000 *60 *60;
-    const minInMs = 1000 *60;
+    let dayInMs = 1000 * 60 * 60 *24;
+    let hourInMs = 1000 *60 *60;
+    let minInMs = 1000 *60;
 
-    const dayEvent = Math.floor(distanceEvent / dayInMs);
-    const hoursEvent = Math.floor((distanceEvent % dayInMs) / hourInMs);
-    const minuteEvent = Math.floor((distanceEvent % hourInMs) / minInMs);
-    const segEvent = Math.floor((distanceEvent % minInMs) / 1000);
+    let dayEvent = Math.floor(distanceEvent / dayInMs);
+    let hoursEvent = Math.floor((distanceEvent % dayInMs) / hourInMs);
+    let minuteEvent = Math.floor((distanceEvent % hourInMs) / minInMs);
+    let segEvent = Math.floor((distanceEvent % minInMs) / 1000);
 
     document.getElementById('count').innerHTML = `${dayEvent}d ${hoursEvent}h ${minuteEvent}m ${segEvent}s`
 
